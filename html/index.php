bf5766a8cf8e2b3694dfb99c55d0d3a45736b7b5
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-10-06 14:44:03 +0200

    updated

--- html/index.php
+++ html/index.php
@@ -4,20 +4,6 @@
 	<title>Home</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="general.css">
-	<style>
-		body {
-			background-color: #202020;
-			display: flex;
-			flex-direction: column;
-		}
-		h1 {
-			color: green;
-			text-align: center;
-			font-size: 55px;
-			text-decoration: underline;
-			margin: 0 auto;
-		}
-	</style>
 </head>
 <body>
 	<div class="horisontal">
@@ -30,18 +16,17 @@
 				<a href="">About</a>
 			</div>
 		</div>
-		<h1>The GreenLife</h1>
-		<div class="login-form">
-			<form action="">
-				<input type="text" name="uid" placeholder="Username/e-mail">
-				<input type="password" name="pwd" placeholder="Password">
-				<button type="submit" name="submit" class="dropbtn">Login</button>
-			</form>
-			<a href="signup.php">Sign Up</a>
-		</div>
+	</div>
+	<div class="login-div">
+		<form class="login-form">
+			<input type="text" name="uid" placeholder="Username/e-mail" class="login-input">
+			<input type="password" name="pwd" placeholder="Password" class="login-input">
+			<button type="submit" name="submit" class="login-button">Login</button>
+		</form>
+		<a href="signup.php" class="login-a">Sign Up</a>
 	</div>
 	<div class="home-img-div">
-		<img src="https://raw.githubusercontent.com/jpirzent/img/master/Cannabis_leaf.svg.png" title="Products" alt="Products" class="home-img">
+		<img src="https://raw.githubusercontent.com/jpirzent/img/master/Cannabis_leaf.svg.png" class="home-img">
 	</div>
 </body>
 </html>
