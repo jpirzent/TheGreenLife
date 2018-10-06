b5e62f0cae5c0af782653a5c3ceda747b13f8329
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-10-06 13:49:57 +0200

    updated

--- html/index.php
+++ html/index.php
@@ -7,11 +7,13 @@
 	<style>
 		body {
 			background-color: #202020;
+			display: flex;
+			flex-direction: column;
 		}
 		h1 {
 			color: green;
 			text-align: center;
-			font-size: 60px;
+			font-size: 55px;
 			text-decoration: underline;
 			margin: 0 auto;
 		}
@@ -21,6 +23,7 @@
 	<div class="horisontal">
 		<div class="dropmenu">
 			<button class="dropbtn">Home</button>
+			<a href="index.php"></a>
 			<div class="dropcontnt">
 				<a href="">Products</a>
 				<a href="">Contact Us</a>
@@ -28,6 +31,17 @@
 			</div>
 		</div>
 		<h1>The GreenLife</h1>
+		<div class="login-form">
+			<form action="">
+				<input type="text" name="uid" placeholder="Username/e-mail">
+				<input type="password" name="pwd" placeholder="Password">
+				<button type="submit" name="submit" class="dropbtn">Login</button>
+			</form>
+			<a href="signup.php">Sign Up</a>
+		</div>
+	</div>
+	<div class="home-img-div">
+		<img src="https://raw.githubusercontent.com/jpirzent/img/master/Cannabis_leaf.svg.png" title="Products" alt="Products" class="home-img">
 	</div>
 </body>
 </html>
