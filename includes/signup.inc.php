576eee729b28412dfe57d36bc6ed84e06b731e3a
James PIRZENTHAL <jpirzent@e5r4p6.wethinkcode.co.za>
2018-10-06 20:30:15 +0200

    code is done, just error checks

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -11,8 +11,11 @@
 		$pwd = mysqli_real_escape_string($conn, $_POST['first']);
 
 		//ERROR HANDLERS
-		//=>check for emp ty fields
-		//=>check if input chars are valid
+		//=>check for empty fields
+		//=>check if first and last is valid
+		//=>check if email is valid
+		//=>hash password
+		//=>insert user into database
 		if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd))
 		{
 			header("Location: ../signup.php?signup=empty");
@@ -20,7 +23,39 @@
 		}
 		else
 		{
+			if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last))
+			{
+				header("Location: ../signup.php?signup=invalid-names");
+				exit();
+			}
+			else
+			{
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+				{
+					header("Location: ../signup.php?signup=invalid-email");
+					exit();
+				}
+				else
+				{
+					$sql = "SELECT FROM users WHERE user_uid='$uid'";
+					$res = mysqli_query($conn, $sql);
+					$resCheck = mysqli_num_rows($res);
 
+					if ($resCheck > 0)
+					{
+						header("Location: ../signup.php?signup=invalid-username");
+						exit();
+					}
+					else
+					{
+						$hashpwd = password_hash($pwd, PASSWORD_DEFAULT);
+						$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashpwd');";
+						mysqli_query($conn, $sql);
+						header("Location: ../signup.php?signup=success");
+						exit();
+					}
+				}
+			}
 		}
 	}
 	else
