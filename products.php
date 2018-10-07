3db0866e8da680afcd390586359db62502a39707
James PIRZENTHAL <jpirzent@e5r4p6.wethinkcode.co.za>
2018-10-07 05:58:38 +0200

    cats done

--- products.php
+++ products.php
@@ -1,32 +1,16 @@
 <?php
     include_once 'header.php';
-    include_once 'includes/dbh.inc.php';
-    $sql = "SELECT * FROM products";
-    $query = mysqli_query($conn, $sql);
-    $rowcnt = mysqli_num_rows($query);
-    $sql = "SELECT * FROM products WHERE pr_type='sativa'";
-    $query = mysqli_query($conn, $sql);
 ?>
-<table class="table-contents">
-    <?php
-        for ($i=0; $i <= $rowcnt; $i++)
-        {
-            if ($row = mysqli_fetch_assoc($query))
-            {
-                echo "<tr>";
-                echo "<td class=\"tbl-div\">";
-                echo "<h1 class=\"tbl-h1\">".$row['pr_name']."</h1>";
-                echo "<div><img src=".$row['pr_picture']." class=\"tbl-img\" ></div>";
-                echo "<div class=\"tbl-p\"><p>".$row['pr_description']."</p></div>";
-                echo "<div class=\"tbl-price\">";
-                echo "<h2>".$row['pr_price']."<h2>";
-                echo "<button type\"submit\" name=\"submit\">Add to Cart</button>";
-                echo "</div>";
-                echo "</td>";
-            }
-        }
-    ?>
-</table>
+
+    <div class="cat-sativa">
+        <h1>Catagories</h1>
+        <ul>
+            <a href="all.php">○       All</a><br />
+            <a href="sativa.php">○        Sativas</a><br />
+            <a href="indica.php">○        Indicas</a><br />
+        </ul>
+    </div>
+
 <?php
     include_once 'footer.php';
 ?>
---
+++ sativa.php
@@ -0,0 +1,39 @@
+<?php
+    include_once 'header.php';
+    include_once 'includes/dbh.inc.php';
+    $sql = "SELECT * FROM products";
+    $query = mysqli_query($conn, $sql);
+    $rowcnt = mysqli_num_rows($query);
+    $sql = "SELECT * FROM products WHERE pr_type='sativa'";
+    $query = mysqli_query($conn, $sql);
+?>
+<table class="table-contents">
+    <?php
+        if (isset($_SESSION['u_id']))
+        {
+            for ($i=0; $i <= $rowcnt; $i++)
+            {
+                if ($row = mysqli_fetch_assoc($query))
+                {
+                    echo "<tr>";
+                    echo "<td class=\"tbl-div\">";
+                    echo "<h1 class=\"tbl-h1\">".$row['pr_name']."</h1>";
+                    echo "<div><img src=".$row['pr_picture']." class=\"tbl-img\" ></div>";
+                    echo "<div class=\"tbl-p\"><p>".$row['pr_description']."</p></div>";
+                    echo "<div class=\"tbl-price\">";
+                    echo "<h2>".$row['pr_price']."<h2>";
+                    echo "</div>";
+                    echo "<button type\"submit\" name=\"submit\">Add to Cart</button>";
+                    echo "</td>";
+                }
+            }
+        }
+        else
+        {
+            echo "<h1 class=\"else-h1\">Please Login to See Our Products</h1>";
+        }
+    ?>
+</table>
+<?php
+    include_once 'footer.php';
+?>
